bb083a66fd6ae44691a17fb696dba67046194945
Zycrasion <hendrix.may@gmail.com>
2023-11-01 21:29:21 +1100

    indexed meshes

--- examples/pong.rs
+++ examples/pong.rs
@@ -1,16 +1,25 @@
 use prospect::{
-    abstraction::{prospect_window::ProspectWindow, graphics_context::GraphicsContext, high_level_abstraction::HighLevelGraphicsContext, vertex::Vertex, mesh::Mesh},
+    abstraction::{prospect_window::ProspectWindow, graphics_context::GraphicsContext, high_level_abstraction::HighLevelGraphicsContext, vertex::Vertex, mesh::{Mesh, Meshable, MeshIndexed}},
     prospect_app::{ProcessResponse, ProspectApp, ProspectEvent},
 };
 use wgpu::{SurfaceError, BufferUsages};
 use wgpu::util::DeviceExt;
 use winit::{event::VirtualKeyCode, window};
 
-const VERTICES : &[Vertex] = &[
-    Vertex { position : [0.0, 0.5, 0.0], colour : [1.0, 0.0, 0.0] },
-    Vertex { position : [-0.5, -0.5, 0.0], colour : [0.0, 1.0, 0.0] },
-    Vertex { position : [0.5, -0.5, 0.0], colour : [0.0, 0.0, 1.0] }
+const VERTICES: &[Vertex] = &[
+    Vertex { position: [-0.0868241, 0.49240386, 0.0], colour: [0.5, 0.5, 0.] }, // A
+    Vertex { position: [-0.49513406, 0.06958647, 0.0], colour: [0.5, 0.0, 0.] }, // B
+    Vertex { position: [-0.21918549, -0.44939706, 0.0], colour: [0.5, 0.0, 0.5] }, // C
+    Vertex { position: [0.35966998, -0.3473291, 0.0], colour: [0., 0.5, 0.] }, // D
+    Vertex { position: [0.44147372, 0.2347359, 0.0], colour: [0., 0.0, 0.5] }, // E
 ];
+
+const INDICES: &[u16] = &[
+    0, 1, 4,
+    1, 2, 4,
+    2, 3, 4,
+];
+
 
 fn main() {
     let window = ProspectWindow::new("Pong", 480, 480);
@@ -22,14 +31,14 @@ fn main() {
 pub struct PongApp
 {
     clear_col : (f64, f64, f64),
-    triangle_mesh : Mesh
+    triangle_mesh : Box<dyn Meshable>
 }
 
 impl PongApp
 {
     fn new(window : &ProspectWindow) -> Self
     {
-        let triangle_mesh = Mesh::from_vertices(VERTICES, window.get_device());
+        let triangle_mesh = Box::new(MeshIndexed::new(VERTICES, INDICES, window.get_device()));
 
         Self
         {
@@ -45,8 +54,10 @@ impl ProspectApp for PongApp {
     fn draw(&mut self, window : &ProspectWindow) -> Result<(), SurfaceError>
     {
         let clear_colour = (self.clear_col.0 / window.size.0 as f64, self.clear_col.1 / window.size.1 as f64, 0.5);
+        
         let (output, view, mut command_encoder) = HighLevelGraphicsContext::init_view(window);
         let mut render_pass = HighLevelGraphicsContext::start_render(clear_colour, &view, &mut command_encoder);
+
         render_pass.set_pipeline(window.get_render_pipeline());
         self.triangle_mesh.draw(&mut render_pass);
 
--- src/abstraction/mesh.rs
+++ src/abstraction/mesh.rs
@@ -1,7 +1,12 @@
 use bytemuck::NoUninit;
-use wgpu::{Buffer, BufferUsages, RenderPass};
+use wgpu::{Buffer, BufferUsages, RenderPass, Device, IndexFormat};
 
 use super::{vertex::Vertex, graphics_context::GraphicsContext};
+
+pub trait Meshable
+{
+    fn draw<'life>(&'life self, render_pass : &mut RenderPass<'life>);
+}
 
 pub struct Mesh
 {
@@ -26,10 +31,52 @@ impl Mesh
             vertex_count 
         }
     }
+}
 
-    pub fn draw<'life>(&'life self, render_pass : &mut RenderPass<'life>)
+impl Meshable for Mesh
+{
+    fn draw<'life>(&'life self, render_pass : &mut RenderPass<'life>)
     {
         render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
         render_pass.draw(0..self.vertex_count, 0..1);
+    }
+}
+
+#[derive(Debug)]
+pub struct MeshIndexed
+{
+    vertex_buffer : Buffer,
+    index_buffer : Buffer,
+    index_count : u32
+}
+
+impl MeshIndexed
+{
+    pub fn new<T, U>(vertices : T, indices : U, device : &Device) -> Self 
+        where   T : Into<Vec<Vertex>>,
+                U : Into<Vec<u16>>
+    {
+        let vertices = vertices.into();
+        let indices = indices.into();
+        let count = indices.len();
+
+        let vertex_buffer = GraphicsContext::create_buffer(device, "Vertex Buffer: MeshIndexed", &vertices, BufferUsages::VERTEX);
+        let index_buffer = GraphicsContext::create_buffer(device, "Index Buffer: MeshIndexed", &indices, BufferUsages::INDEX);
+
+        Self
+        {
+            vertex_buffer,
+            index_buffer,
+            index_count: count as u32
+        }
+    }
+}
+
+impl Meshable for MeshIndexed
+{
+    fn draw<'life>(&'life self, render_pass : &mut RenderPass<'life>) {
+        render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+        render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);  
+        render_pass.draw_indexed(0..self.index_count, 0, 0..1); 
     }
 }
